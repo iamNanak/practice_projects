3e516cd3129b5117bd94a614dfda4aa38c1c81bc
Nanak <iamnanak01@gmail.com>
2024-07-10 14:49:30 +0530

    add & learn

--- website2/script.js
+++ website2/script.js
@@ -2,3 +2,20 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+let fix = document.querySelector('#fixed')
+let elem = document.querySelector('#element-container')
+
+elem.addEventListener('mouseenter', function(){
+    fix.style.display = 'block'
+})
+elem.addEventListener('mouseleave',function(){
+    fix.style.display = 'none'
+})
+document.querySelectorAll('.element').forEach(function(e){
+    e.addEventListener('mouseenter',function(){
+        const image = e.getAttribute('data-image')
+         fix.style.backgroundImage = `url(${image})`
+    })
+})
